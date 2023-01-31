7c6d4175f3a688ff17d1129f42c83e60eb37c4fb
Zach Watkins <zwatkins31@gmail.com>
2023-01-30 16:32:20 -0800

    Updated files

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,38 @@
-import React from "react";
-// import { Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import Fade from "react-reveal/Fade";
 
 const HeroSection = () => {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+    }
+  }, []);
+
+  const handleClick = () => {
+    if (isDesktop === true) {
+      console.log("Desktop!");
+    } else {
+      console.log("Mobile!");
+    }
+  };
+
   return (
-    <h1 className="hero-title">
-      {`Hi! My name is `}
-      <span className="gradient-text-color-main">Zach.</span>
-      <br />
-      I'm a full stack developer.
-    </h1>
+    <section id="hero" className="jumbotron">
+      <Container>
+        <Fade left={true} distance="30px" duration={1000} delay={500}>
+          <h1 className="hero-title">
+            {`Hi! My name is `}
+            <span className="gradient-text-color-main">Zach.</span>
+            <br />
+            {`I'm a full stack developer.`}
+          </h1>
+          <button onClick={handleClick}>Click Me!</button>
+        </Fade>
+      </Container>
+    </section>
   );
 };
 
