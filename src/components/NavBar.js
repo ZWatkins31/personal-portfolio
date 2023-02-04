e0a79f992921668ab22d69fa4d71ad9cc9317137
Zach Watkins <zwatkins31@gmail.com>
2023-02-04 12:49:05 -0800

    Updated files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,14 +12,14 @@ const NavBar = () => {
   return (
     <Navbar expand="md">
       <Container>
-        <Navbar.Brand href="#home">
-          <img src={""} alt="Logo" />
+        <Navbar.Brand className="navbar-logo" href="#home">
+          <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#skills">Skills</Nav.Link>
@@ -28,7 +28,7 @@ const NavBar = () => {
           <span className="navbar-text">
             <div className="social-icons">
               <a href="#">
-                <Linkedin />
+                <Linkedin color="background" />
               </a>
               <a href="#">
                 <Github />
