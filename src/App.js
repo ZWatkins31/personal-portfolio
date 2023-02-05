ee7707017c1fe5dbebf615d99c7eb1a63081ac4a
Zach Watkins <zwatkins31@gmail.com>
2023-02-05 11:42:34 -0800

    Updated files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <HeroSection />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const About = () => {
+  return (
+    <section id="about">
+      <Container>
+        <h1>About Me:</h1>
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
+            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
+            voluptatum consequatur blanditiis inventore debitis fuga numquam
+            voluptate architecto itaque molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
+            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
+            voluptatum consequatur blanditiis inventore debitis fuga numquam
+            voluptate architecto itaque molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
+            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
+            voluptatum consequatur blanditiis inventore debitis fuga numquam
+            voluptate architecto itaque molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
+            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
+            voluptatum consequatur blanditiis inventore debitis fuga numquam
+            voluptate architecto itaque molestiae.
+          </p>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
