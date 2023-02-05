53b9761bb399b990cd0b57365de19da722c70812
Zach Watkins <zwatkins31@gmail.com>
2023-02-05 14:44:35 -0800

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 // import "./styles/main.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
