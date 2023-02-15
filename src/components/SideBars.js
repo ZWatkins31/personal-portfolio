5cd03c043c5015882b6256750d01ff495025be82
Zach Watkins <zwatkins31@gmail.com>
2023-02-15 10:02:06 -0800

    Updated files

--- src/components/SideBars.js
+++ src/components/SideBars.js
@@ -5,20 +5,23 @@ const SideBars = () => {
   return (
     <div>
       <div className="sidebar-left">
-        <a href="#">
-          <Linkedin size={30} />
-        </a>
-        <a href="#">
-          <Github size={30} />
-        </a>
-        <a href="#">
-          <FileEarmarkPersonFill size={30} />
-        </a>
+        <div className="sb-social-icons">
+          <a href="#">
+            <Linkedin size={25} />
+          </a>
+          <a href="#">
+            <Github size={25} />
+          </a>
+          <a href="#">
+            <FileEarmarkPersonFill size={25} />
+          </a>
+        </div>
         <div className="vert-line"></div>
       </div>
 
       <div className="sidebar-right">
         <div className="vert-line"></div>
+        <a href="#">zwatkins31@gmail.com</a>
       </div>
     </div>
   );
