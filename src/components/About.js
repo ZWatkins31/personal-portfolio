8b448cba49efdfee20e29d73893a05309ca57f10
Zach Watkins <zwatkins31@gmail.com>
2023-02-18 10:29:22 -0800

    Updated files

--- src/components/About.js
+++ src/components/About.js
@@ -10,43 +10,24 @@ const About = () => {
           <p>
             Hi there! My name is Zach and I enjoy building accessible,
             functional, and creative web pages and applications. Along with
-            being full stack developer and software engineer I also have a
+            being full stack developer and software engineer, I also have a
             background in mechanical engineering.
           </p>
           <p>
-            Always looking to push myself to learn new things and skills to
-            become a well-rounded engineer, My journey as software engineer
-            began in 2021 when I began
+            Passionate about learning and always looking pushing myself to pick
+            up new skills, my journey into tech began in 2021 when I began
+            teaching myself to how to code. Almost immediately, I knew this is
+            what I wanted to do with my life and to further immerse myself in
+            the subject I decided to enroll in Thinkful's Software Engineering
+            Program where I quickly absorbed new concepts, while bringing
+            ambitious ideas and a positive attitude to every challenge. I truly
+            love the combination of creative and logical problem-solving skills
+            I get to use when writing code and I love how there's always more to
+            learn!
           </p>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
-            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
-            voluptatum consequatur blanditiis inventore debitis fuga numquam
-            voluptate architecto itaque molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
-            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
-            voluptatum consequatur blanditiis inventore debitis fuga numquam
-            voluptate architecto itaque molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
-            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
-            voluptatum consequatur blanditiis inventore debitis fuga numquam
-            voluptate architecto itaque molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
-            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
-            voluptatum consequatur blanditiis inventore debitis fuga numquam
-            voluptate architecto itaque molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
-            neque, ipsa animi maiores repellendu distinctioaperiam earum dolor
-            voluptatum consequatur blanditiis inventore debitis fuga numquam
-            voluptate architecto itaque molestiae.
+            Whether it's hiking, biking, skiing, or just going on a stroll, when
+            I'm not coding you can typically find me exploring the outdoors.
           </p>
         </div>
       </Container>
